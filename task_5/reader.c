5296bf7d0d0c8417d42444f2fc186692549132b5
fasSstem <fedorov.ar@phystech.edu>
2020-11-26 14:18:41 +0300

    Добавил 5ое задание, нужно запустить reader, запустить sender, передать sender id to reader, и файл input перезапишется в output.

--- task_5/reader.c
+++ task_5/reader.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <ctype.h>
+
+int i;
+int flag = 0;
+void sig_handler(int signo)
+{
+	if (signo == SIGUSR1)
+	{
+		i = 0;
+		flag = 1;
+	}
+	if (signo == SIGUSR2)
+	{
+		i = 1;
+		flag = 1;
+	}
+}
+
 int main()
 {
-	int fd = open("testfile2", O_RDWR);
-	int counter = 0;
-	int i;
-	int d = 0;
-	while ()//priem 1/0
+	printf("My pid is %d\n", getpid());
+	if (signal(SIGUSR1, sig_handler) == SIG_ERR)
 	{
-		d += i << (7 - counter); 
-		counter++;
-		if (counter == 8)
+		printf("Can't catch SIGUSR1\n");
+	}
+	if(signal(SIGUSR2, sig_handler) == SIG_ERR)
+	{
+		printf("Can't catch SIGUSR2\n");
+	}
+	int d = 0;
+	int counter = 0;
+	int fd = open("output", O_RDWR);
+	while (1)//priem 1/0
+	{
+		if (flag == 1)
 		{
-			counter = 0;
-			char dd[1];
-			dd[0] = d;
-			write(fd, dd, 1);
-			d = 0;
+			flag = 0;
+			d += i << (7 - counter); 
+			counter++;
+			if (counter == 8)
+			{
+				counter = 0;
+				char dd[1];
+				dd[0] = d;
+				write(fd, dd, 1);
+				d = 0;
+			}
 		}
 	}
 }
--- task_5/sender.c
+++ task_5/sender.c
@@ -4,15 +4,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <signal.h>
+#include <time.h>
 int main()
 {
-	int fd = open("testfile", O_RDWR);
+	struct timespec tw = {0, 1000000};
+
+	int id;
+	printf("Give me id:");
+	scanf("%d", &id);
+	printf("Thx\n");
+	int fd = open("input", O_RDWR);
 	char c[1];
 	while (read(fd, c, 1) > 0)
 	{
 		for (int i = 0; i < 8; i++)
 		{
 			int a = (c[0] >> (7 - i)) & 1;
+			//printf("%d", a);
+			if (a == 0)
+			{
+				kill(id, SIGUSR1);
+			}
+			else
+			{
+				kill(id, SIGUSR2);
+			}
+			nanosleep(&tw, NULL);
 		}
 	}
+	kill(id, SIGINT);
 }
