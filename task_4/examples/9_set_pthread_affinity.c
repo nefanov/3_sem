716f5aed98323aae6b767eeadae4e832a996708a
nefanov <efanov@phystech.edu>
2020-12-15 21:14:06 +0000

    Create 9_set_pthread_affinity.c

---
+++ task_4/examples/9_set_pthread_affinity.c
@@ -0,0 +1,16 @@
+#include <sched.h>
+#define _GNU_SOURCE
+// core_id = 0, 1, ... n-1, where n is the system's number of cores
+
+int stick_this_thread_to_core(int core_id) {
+   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+   if (core_id < 0 || core_id >= num_cores)
+      return EINVAL;
+
+   cpu_set_t cpuset;
+   CPU_ZERO(&cpuset);
+   CPU_SET(core_id, &cpuset);
+
+   pthread_t current_thread = pthread_self();    
+   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+}
